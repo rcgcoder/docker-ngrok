12e4671203021bea714ed2ad8c964d6ff34d9caf
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-06 07:09:08 +0100

    change exec system

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -e
 
+ARGS="ngrok"
 file="/opt/ngrok/conf/ngrok.yml"
 if [ -f "$file" ]
 then
-	exec "/bin/ngrok start -config=/opt/ngrok/conf/ngrok.yml default_tunnel"
+	ARGS="$ARGS start -config=/opt/ngrok/conf/ngrok.yml default_tunnel"
 else
-	exec "/bin/ngrok start -config=/home/ngrok/.ngrok2/ngrok.yml default_tunnel"
+	ARGS="$ARGS start -config=/home/ngrok/.ngrok2/ngrok.yml default_tunnel"
 fi
+
+set -x
+exec $ARGS
