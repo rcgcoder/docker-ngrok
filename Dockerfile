660b7f165a58beaf52b745b658517d0b37788034
rcantabrana <rcantabrana@A0108030.ARAGON>
2018-03-06 06:59:37 +0100

    exec with /bin/

--- Dockerfile
+++ Dockerfile
@@ -25,5 +25,5 @@ USER ngrok
 EXPOSE 4040
 
 WORKDIR /opt/ngrok/conf
-RUN /bin/sh
-#CMD ["/entrypoint.sh"]
+#RUN /bin/sh
+CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 file="/opt/ngrok/conf/ngrok.yml"
 if [ -f "$file" ]
 then
-	exec "ngrok start -config=/opt/ngrok/conf/ngrok.yml default_tunnel"
+	exec "/bin/ngrok start -config=/opt/ngrok/conf/ngrok.yml default_tunnel"
 else
-	exec "ngrok start -config=/home/ngrok/.ngrok2/ngrok.yml default_tunnel"
+	exec "/bin/ngrok start -config=/home/ngrok/.ngrok2/ngrok.yml default_tunnel"
 fi
